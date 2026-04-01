a6f4ec657e89f5097cd14e1406e3137c7ea658fe
cryder15 <chad.ryder15@gmail.com>
2026-04-01 16:30:12 -0500

    Update dependencies to work with neovim 0.12.0

--- nvim/lua/plugins/diagram.lua
+++ nvim/lua/plugins/diagram.lua
@@ -1,6 +1,16 @@
+local function kitty_graphics_supported()
+  local term = vim.env.TERM or ''
+  local term_program = vim.env.TERM_PROGRAM or ''
+  return term:find 'kitty' ~= nil
+    or term_program == 'ghostty'
+    or term_program == 'WezTerm'
+    or vim.env.GHOSTTY_RESOURCES_DIR ~= nil
+end
+
 return {
   {
     '3rd/diagram.nvim',
+    cond = kitty_graphics_supported,
     dependencies = { '3rd/image.nvim' },
     opts = {
       renderer_options = {
@@ -13,12 +23,13 @@ return {
   },
   {
     '3rd/image.nvim',
+    cond = kitty_graphics_supported,
     opts = {
-      backend = 'kitty', -- Best for Ghostty
+      backend = 'kitty',
       integrations = {
         markdown = {
           enabled = true,
-          clear_in_insert_mode = true, -- Cleaner editing experience
+          clear_in_insert_mode = true,
           filetypes = { 'markdown', 'vimwiki' },
         },
       },
