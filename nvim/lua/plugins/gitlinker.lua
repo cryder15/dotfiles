1aacf75bc0979e88bb67e337464e9775a1225848
cryder15 <chad.ryder15@gmail.com>
2026-04-14 09:20:28 -0500

    Swap gitlinker

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -1,13 +1,9 @@
 return {
-  'ruifm/gitlinker.nvim',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
+  'linrongbin16/gitlinker.nvim',
+  cmd = 'GitLink',
+  opts = {},
+  keys = {
+    { '<leader>yg', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
+    { '<leader>yG', '<cmd>GitLink!<cr>', mode = { 'n', 'v' }, desc = 'Open git link in browser' },
   },
-  config = function()
-    require('gitlinker').setup {
-      -- Set your custom keybind here
-      mappings = '<leader>yg',
-    }
-  end,
 }
-
